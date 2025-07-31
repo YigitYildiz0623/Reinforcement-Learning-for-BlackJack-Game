dfc2f563bbc4e4e666eecb4450e6051fb5328066
YigitYildiz0623 <125692781+YigitYildiz0623@users.noreply.github.com>
2025-07-31 19:23:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,32 +4,32 @@ from game_environment import BlackJackEnv
 q_table = np.load('q_table.npy')
 env = BlackJackEnv()
 
-num_test = 50000
+num_test = 10000
 wins = 0
 loses = 0
 draws = 0
 
 for _ in range(num_test):
-    state = env.reset_game()
+    player_score, dealer_card, usable_ace = env.reset_game()
+    usable_ace = int(usable_ace)
     game_over = False
-    
-    while not game_over:
-        player_score, dealer_hand = state
-        action = np.argmax(q_table[player_score,dealer_hand])
 
-        next_state, reward, game_over,_ = env.step(action)
-        state = next_state
+    while not game_over:
+        action = np.argmax(q_table[player_score, dealer_card, usable_ace, :])
+        next_state, reward, game_over, _ = env.step(action)
+        player_score, dealer_card, usable_ace = next_state
+        usable_ace = int(usable_ace)
 
     if reward == 1:
-        wins +=1
+        wins += 1
     elif reward == -1:
-        loses +=1
+        loses += 1
     else:
-        draws +=1
+        draws += 1
 
-win_rate = wins/num_test
-lose_rate = loses/num_test
-draw_rate = draws/num_test
+win_rate = wins / num_test
+lose_rate = loses / num_test
+draw_rate = draws / num_test
 
 print('Testler Bitti')
 print('-')
